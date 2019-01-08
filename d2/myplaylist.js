c51b273e7f676b8729afb1478f9b6b6ad53ee021
Artemij <artemij91@outlook.com>
2019-01-08 01:19:28 +0100

    first commit

---
+++ d2/myplaylist.js
@@ -0,0 +1,13 @@
+var myPlaylist = [
+
+    {
+        mp3:'music/dt3.wav',
+        title:'dt3',
+        artist:'Artemij',
+    },
+    {
+        mp3:'music/m2.5.wav',
+        title:'m2.5',
+        artist:'Artemij',
+    },
+];
