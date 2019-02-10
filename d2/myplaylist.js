ac30bfd782a1ad0ddd21d2a0a498e7089c6e7761
Artemij <artemij91@outlook.com>
2019-02-10 06:01:15 +0100

    add song

--- d2/myplaylist.js
+++ d2/myplaylist.js
@@ -1,8 +1,18 @@
 var myPlaylist = [
 
     {
+        mp3:'music/dt2.wav',
+        title:'cover_dt2',
+        artist:'Artemij',
+    },
+    {
         mp3:'music/dt3.wav',
-        title:'dt3',
+        title:'cover_dt3',
+        artist:'Artemij',
+    },
+    {
+        mp3:'music/cover_piano_R2_1.wav',
+        title:'cover_r1',
         artist:'Artemij',
     },
 ];
